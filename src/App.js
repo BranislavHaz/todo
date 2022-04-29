cc7f8187fa91acd200e19b3d57bd4a604c318ddb
Branislav Ház <haz.branislav@gmail.com>
2022-04-29 17:15:12 +0200

    Create empty structure Redux.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import AddListForm from "./components/AddListForm";
 import "./App.css";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-      <AddListForm />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>Hello</h1>
+        <AddListForm />
+      </div>
+    </Provider>
   );
 }
 
